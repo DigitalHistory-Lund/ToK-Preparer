08f59ef1af521daf43006c73941951a401f2cd34
Mathias Johansson <MathiasJoha@gmail.com>
2026-05-31 13:50:14 +0200

    homogenize chamber db creation

--- src/prepare_chamber_dbs.py
+++ src/prepare_chamber_dbs.py
@@ -1,28 +1,32 @@
 from .settings import tmp_db, tmp_db1, tmp_db2
 import sqlite3
 import shutil
+def make_chamber_file(number: int, force=False):
+    assert number in {1, 2}
+    target_file = [tmp_db1, tmp_db2][number - 1]
+    assert not force and not target_file.exists(), (
+        f"{target_file=}\t{target_file.exists()}"
+    )
 
+    shutil.copyfile(tmp_db, target_file)
+
+    with sqlite3.connect(tmp_db1) as conn:
+        conn.execute(f"DELETE FROM utterance where kammare != {number}")
+        conn.commit()
 
 
 def make_chamber_1():
-    assert not tmp_db1.exists(), f"{tmp_db1=}\t{tmp_db1.exists()}"
+    make_chamber_file(1)
 
-    with sqlite3.connect(tmp_db1) as conn:
-        conn.execute('DELETE FROM utterance where kammare != 1')
-        conn.commit()
 
 def make_chamber_2():
-    assert not tmp_db2.exists(), f"{tmp_db2=}\t{tmp_db2.exists()}"
+    make_chamber_file(2)
 
-    with sqlite3.connect(tmp_db2) as conn:
-        conn.execute('DELETE FROM utterance where kammare != 2')
-        conn.commit()
 
-if __name__ == '__main__':
-    assert not (tmp_db1.exists() or tmp_db2.exists()), f"\n{tmp_db1=}\t{tmp_db1.exists()}\n{tmp_db2=}\t{tmp_db2.exists()}"
-
-    shutil.copyfile(tmp_db, tmp_db1)
-    shutil.copyfile(tmp_db, tmp_db2)
+if __name__ == "__main__":
+    assert not (tmp_db1.exists() or tmp_db2.exists()), (
+        f"\n{tmp_db1=}\t{tmp_db1.exists()}\n{tmp_db2=}\t{tmp_db2.exists()}"
+    )
 
     make_chamber_1()
     make_chamber_2()
